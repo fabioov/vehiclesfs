f5d45505a774c62c1508dd75d763200f9430d8d1
Fabio Santos <fabioov@hotmail.com>
2023-03-15 16:11:36 -0300

    New Project

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["Display/vehiclesfs/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"Display/vehiclesfs/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
